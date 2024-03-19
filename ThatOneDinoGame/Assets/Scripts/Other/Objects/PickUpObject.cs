2d8b702a93c37d0e51ddf0c3d21c7a5926b6b871
AymenChl312 <aymenchlabi04@gmail.com>
2024-03-19 16:43:58 +0100

    Volume Son/Musique
    Amelioration du code de l'interface Settings
    
    Ajout d'une librairie de SoundEffect temporaire
    
    Debut de code pour SoundEffect (sur les pieces)

--- ThatOneDinoGame/Assets/Scripts/Other/Objects/PickUpObject.cs
+++ ThatOneDinoGame/Assets/Scripts/Other/Objects/PickUpObject.cs
@@ -2,10 +2,15 @@ using UnityEngine;
 
 public class PickUpObject : MonoBehaviour
 {
+    public AudioSource audioSource;
+    public AudioClip sound;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+
         if(collision.CompareTag("Player"))
         {
+            audioSource.PlayOneShot(sound);
             Inventory.instance.AddCoins(1);
             CurrentSceneManager.instance.coinsPickedUpInThisSceneCount++;
             Destroy(gameObject);
--- ThatOneDinoGame/Assets/Scripts/Scene/Menu/MainMenu.cs
+++ ThatOneDinoGame/Assets/Scripts/Scene/Menu/MainMenu.cs
@@ -17,6 +17,8 @@ public class MainMenu : MonoBehaviour
         {
             Screen.fullScreen = false;
         }
+
+
     }
 
     public void StartGame()
